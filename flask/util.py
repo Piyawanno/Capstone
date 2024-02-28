edafb244237462ba1f001bb7fe9e13b2f8bdb892
Kittipong Piyawanno <k.piyawanno@gmail.com>
2024-02-28 13:49:50 +0700

    Flask

--- flask/util.py
+++ flask/util.py
@@ -1,6 +1,11 @@
 from jinja2 import Template
 from typing import Dict, Any
-import json
+
+import json, sqlite3
+
+def connectDB(path: str) -> sqlite3.Connection:
+	connection = sqlite3.connect(path, isolation_level=None, check_same_thread=False)
+	return connection.cursor()
 
 def readConfig() -> Dict[str, Any]:
 	with open('config.json') as fd :
