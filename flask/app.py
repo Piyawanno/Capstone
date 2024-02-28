32fe6f50b3d21bad012e9aa6785dd6c62ab3f0cc
Kittipong Piyawanno <k.piyawanno@gmail.com>
2024-02-28 13:23:42 +0700

    Flask

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from datetime import datetime
+
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-	return "<p>Hello, World!</p>"
+@app.route("/data")
+def getData():
+	# List
+	# Dict
+	# int
+	# float
+	# str
+	data = [
+		123, 'abcde', 20.56, datetime.now().strftime('%Y-%m-%d')
+	]
+	dumped = json.dumps(data)
+	print(type(data))
+	print(type(dumped))
+	return dumped
 
 if __name__ == '__main__':
 	app.run(host='localhost', port=8080)
